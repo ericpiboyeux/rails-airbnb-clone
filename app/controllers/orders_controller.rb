f19156f6a2a753849caba38b88e051456612108c
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2017-02-20 17:39:18 +0100

    orders controller skeleton

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,30 @@
+class OrdersController < ApplicationController
+
+before_action :find_order, only: [:show, :update, :destroy]
+
+def index
+  @orders = Order.all
+end
+
+def create
+  @order = Order.new(order_params)
+    if @order.save
+      redirect_to orders_path(@order)
+    else
+      render :new
+    end
+
+end
+
+
+private
+
+  def find_orders
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:portions)
+  end
+
+end
