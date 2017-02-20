01c139304352063f17ee2c5b06690486b678c533
Eric Piboyeux <ericpiboyeux@gmail.com>
2017-02-20 19:14:50 +0100

    and done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :users
+  has_many :dishes
+  has_many :orders
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :address, presence: true
