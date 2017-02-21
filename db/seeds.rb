af2401ed339871f35457e8c10cb0dd87761abeb1
Eric Piboyeux <ericpiboyeux@gmail.com>
2017-02-21 17:14:32 +0100

    seed data in Bordeaux

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,35 @@
 
 # https://github.com/stympy/faker
 t_index = 0
+address_array = ["248 Rue du Jardin public, 33300 Bordeaux",
+  "127 Cours du Médoc, 33300 Bordeaux",
+  "4 Impasse Cabrière, 33300 Bordeaux",
+  "12 Cité Chantecrit, 33300 Bordeaux",
+  "35 Rue de Leybardie, 33300 Bordeaux",
+  "74 Rue Maurice, 33300 Bordeaux",
+  "60 Rue Borie, 33300 Bordeaux",
+  "108 Rue Barreyre, 33300 Bordeaux",
+  "102 Rue Prunier, 33300 Bordeaux",
+  "14 Rue Etienne Huyard, 33300 Bordeaux",
+  "28 Rue André Bac, 33300 Bordeaux",
+  "18 Rue Charles Puyo, 33300 Bordeaux",
+  "46 Allée Haussmann, 33300 Bordeaux",
+  "40 Rue Armand Dulamon, 33300 Bordeaux",
+  "12 Rue Vieillard, 33300 Bordeaux"
+]
+first_name_array = ["Marie","Jean","Laurence","Karine","André","Anne","Jane"]
+last_name_array = ["pon","du","pierre","doen","fran","sen","dieu","petit"]
 
-20.times do
-  t_index += 1
+15.times do
   user_seed = User.new(
-    firstname: Faker::Name.first_name,
-    lastname: Faker::Name.last_name,
-    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-    email: "test#{t_index}@gmail.com",
+    firstname: first_name_array.sample,
+    lastname: (last_name_array.sample + last_name_array.sample).capitalize,
+    address: address_array[t_index],
+    email: "test#{t_index + 1}@gmail.com",
     password: "password"
   )
   user_seed.save!
+  t_index += 1
   # address: "#{Faker::Address.street_address}, #{Faker::Address.city}"
 
   user_dish_number = (0..5).to_a.sample
@@ -35,10 +53,10 @@ t_index = 0
 
     hour = [12, 20].sample
     day = DateTime.now.day + (1..5).to_a.sample
-    zone = DateTime.now.zone
+    #zone = DateTime.now.zone
 
     availability_seed = Availability.new(
-      available_datetime: DateTime.new(2017, 02, day, hour, 0, 0, zone),
+      available_datetime: DateTime.new(2017, 02, day, hour, 0, 0),
       portions: (1..5).to_a.sample
     )
 
